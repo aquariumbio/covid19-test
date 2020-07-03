b5e29e03ca64a410b6d2fd0d397c2bc142231e54
Devin Strickland <strcklnd@uw.edu>
2020-07-03 16:35:07 -0700

    added data structure, documentation

---
+++ diagnostic_rt_qpcr/libraries/dataassociationkeys/source.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module DataAssociationKeys
+  GROUP_SIZE_KEY = 'group_size'
+  COMPOSITION_NAME_KEY = 'composition_name'
+  MASTER_MIX_KEY = 'master_mix'
+  MASTER_MIX_STOCK_KEY = 'master_mix_stock'
+  PRIMER_PROBE_MIX_KEY = 'primer_probe_mix'
+end
