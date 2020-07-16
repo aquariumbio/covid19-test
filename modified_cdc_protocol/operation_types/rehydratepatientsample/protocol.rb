ea064190d95421b756108fa4f9f65dfad027265e
Cannon Mallory <malloc3@uw.edu>
2020-07-16 15:48:08 -0700

    Address comments from Deving, uses Refactored Tube Rack

--- modified_cdc_protocol/operation_types/rehydratepatientsample/protocol.rb
+++ modified_cdc_protocol/operation_types/rehydratepatientsample/protocol.rb
@@ -65,53 +65,76 @@ class Protocol
 
     safety_warning
 
+    attached_samples_warning
+
     operations.retrieve.make
 
     operations.each do |op|
       operation_params = op.temporary[:options]
       add_qty_display(operation_params)
 
-      op.pass(TEMPLATE)
+      array_pass(op, TEMPLATE)
 
-      item_list = op.input_array(TEMPLATE).map do |fv|
-        fv.item.collection? ? fv.collection : fv.item
-      end
+      item_list = op.input_array(TEMPLATE).map{ |fv| fv.item }
+      media = op.input(MEDIA).item
 
-      item_list.uniq!
-
-      sample_rack = TubeRackFactory.build(item_list: item_list,
-                                  object_type:'96 Well Strip Well Tube Rack')
+      sample_rack = TubeRack.new(8, 12, name: "#{TUBE_MICROFUGE} rack")
 
       show_fetch_tube_rack(sample_rack)
 
       show_add_items(item_list, sample_rack)
 
-      transfer_media_to_rack(sample_rack: sample_rack,
-                                   media: op.input(MEDIA).item,
-                                   volume: operation_params[:media_volume_qty],
-                                   rc_list: sample_rack.get_non_empty)
-      vortex_samples(sample_rack: sample_rack)
+      show_fill_reservoir(media,
+                          operation_params[:media_volume_qty],
+                          item_list.length)
+
+      show_transfer_media_to_rack(tube_rack: sample_rack,
+                                  media: media,
+                                  volume: operation_params[:media_volume_qty],
+                                  rc_list: sample_rack.find_multiple(item_list))
+      vortex_samples(sample_rack: sample_rack,
+                     rc_list: sample_rack.find_multiple(item_list))
     end
     operations.store
 
     {}
   end
-    
+
   # Directions to vortex samples
-  # @aparam sample_rack [SampleRack]
+  # @param sample_rack [SampleRack]
   # @param rc_list [Array<[r,c]>] list of all locations that need media
   def vortex_samples(sample_rack:, rc_list: nil)
-    rc_list = sample_rack.collection.get_non_empty if rc_list.nil?
+    rc_list = sample_rack.get_non_empty if rc_list.nil?
 
     show do
       title 'Vortex Samples'
       note "Individually vortex each #{TUBE_MICROFUGE} tube in"\
-           " <b>#{sample_rack.object_type}-#{sample_rack.id}</b>"
+           " <b>#{sample_rack.name}</b>"
       note "Be sure to return each #{TUBE_MICROFUGE}"\
            ' Tube to the correct location'
-      table highlight_collection_rc(sample_rack.collection, rc_list) { |r, c|
+      table highlight_tube_rack_rc(sample_rack, rc_list) { |r, c|
         sample_rack.part(r, c).id
       }
+    end
+  end
+
+  def array_pass(op, input_name, output_name = nil)
+    input_fv_array = op.input_array(input_name)
+    output_fv_array = op.output_array(output_name || input_name)
+
+    input_fv_array.zip(output_fv_array).each do |fv_in, fv_out|
+      fv_out.child_sample_id = fv_in.child_sample_id
+      fv_out.child_item_id = fv_in.child_item_id
+      fv_out.save
+    end
+  end
+
+  def attached_samples_warning
+    show do
+      title 'Attached Items'
+      note 'Some items may be physically attached together in stripwells'
+      note 'Cut these items apart as needed'
+      warning 'Be sure to return any unused items to their proper locations'
     end
   end
 end
