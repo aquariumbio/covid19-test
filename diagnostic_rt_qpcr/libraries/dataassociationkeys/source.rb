bb9f612346af7c429b331c3320ff721e712aff93
Devin Strickland <dvnstrcklnd@users.noreply.github.com>
2020-07-24 12:08:15 -0700

    Update source.rb

--- diagnostic_rt_qpcr/libraries/dataassociationkeys/source.rb
+++ diagnostic_rt_qpcr/libraries/dataassociationkeys/source.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 module DataAssociationKeys
-  PRIMER_GROUP_SIZE_KEY = 'primer_group_size'
   COMPOSITION_NAME_KEY = 'composition_name'
+  PRIMER_GROUP_SIZE_KEY = 'primer_group_size'
+  PRIMER_METHOD_KEY = 'primer_method'
+  SAMPLE_GROUP_SIZE_KEY = 'sample_group_size'
+  SAMPLE_METHOD_KEY = 'sample_method'
   MASTER_MIX_KEY = 'master_mix'
   MASTER_MIX_STOCK_KEY = 'master_mix_stock'
   PRIMER_PROBE_MIX_KEY = 'primer_probe_mix'
   TEMPLATE_KEY = 'template'
-  SAMPLE_GROUP_SIZE_KEY = 'sample_group_size'
-  SAMPLE_METHOD_KEY = 'sample_method_key'
-  RAW_QPCR_DATA_KEY = 'RAW qPCR File'
-  PRIMER_METHOD_KEY = 'layout_method_key'
-  LOT_NUM_KEY = 'Lot Number'
+  RAW_QPCR_DATA_KEY = 'raw_qpcr_data'
+  LOT_NUM_KEY = 'lot_number'
 end
