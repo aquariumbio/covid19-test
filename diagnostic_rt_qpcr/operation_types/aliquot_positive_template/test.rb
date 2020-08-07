cff379a547e03e940c59c2f3f040cbff3193c822
Devin Strickland <strcklnd@uw.edu>
2020-08-06 21:22:41 -0700

    added better test for aliquot_positive_template

--- diagnostic_rt_qpcr/operation_types/aliquot_positive_template/test.rb
+++ diagnostic_rt_qpcr/operation_types/aliquot_positive_template/test.rb
@@ -2,11 +2,22 @@
 
 class ProtocolTest < ProtocolTestBase
   def setup
-    add_random_operations(1)
+    # add_random_operations(1)
+
+    sample = Sample.find_by_name('nCoVPC')
+    add_operation
+      .with_input('Template', sample)
+      .with_property(
+        'Options',
+        '{}'
+      )
+      .with_output('Template', sample)
   end
 
   def analyze
     log('Hello from Nemo')
     assert_equal(@backtrace.last[:operation], 'complete')
+    # d = @backtrace[3] #find_display_by_title('Resuspend Positive Template')
+    # log(d[:content].to_s)
   end
 end
