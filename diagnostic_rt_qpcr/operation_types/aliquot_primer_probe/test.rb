f47df011f07db3ec8d9939f382c421771bc36ef6
Devin Strickland <strcklnd@uw.edu>
2020-08-06 21:21:50 -0700

    added better test for aliquot_primer_probe and added missing period

--- diagnostic_rt_qpcr/operation_types/aliquot_primer_probe/test.rb
+++ diagnostic_rt_qpcr/operation_types/aliquot_primer_probe/test.rb
@@ -2,7 +2,23 @@
 
 class ProtocolTest < ProtocolTestBase
   def setup
-    add_random_operations(1)
+    # add_random_operations(1)
+
+    samples = [
+      Sample.find_by_name('2019-nCoV_N1'),
+      Sample.find_by_name('2019-nCoV_N2'),
+      Sample.find_by_name('RP')
+    ]
+
+    samples.each do |sample|
+      add_operation
+        .with_input('Primer/Probe Mix', sample)
+        .with_property(
+          'Options',
+          '{}'
+        )
+        .with_output('Primer/Probe Mix', sample)
+    end
   end
 
   def analyze
