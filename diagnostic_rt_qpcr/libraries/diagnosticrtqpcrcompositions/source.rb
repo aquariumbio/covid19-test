635166cac7042ae7b52edd222d89bb762f2896aa
Cannon Mallory <malloc3@uw.edu>
2020-08-20 08:43:59 -0700

    Incorperation of Devin's edits

--- diagnostic_rt_qpcr/libraries/diagnosticrtqpcrcompositions/source.rb
+++ diagnostic_rt_qpcr/libraries/diagnosticrtqpcrcompositions/source.rb
@@ -23,7 +23,7 @@ module DiagnosticRTqPCRCompositions
       given_stripwells = options[:stripwell_id]
       stripwells = []
       if given_stripwells
-        stripwells = given_stripwells.map { |id| Collection.find(id) }
+        stripwells = Collection.find(given_stripwells)
       else
         stripwells = find_stripwells(sample_names: options[:sample_names],
                                      stripwell_ot: options[:object_type],
